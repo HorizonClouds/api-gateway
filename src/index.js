353f89067176f99a50a69f065da342657d5767ba
Manuel Otero Barbasán <manolo.ob@hotmail.com>
2025-01-10 20:45:51 +0100

    feat: add infrastructure deployment configuration

--- src/index.js
+++ src/index.js
@@ -33,6 +33,21 @@ app.get('/api/v1/infrastructure', (req, res) => {
         return res.json(config.infrastructure);
     }
 });
+
+app.post('/api/v1/infrastructure', express.json(), (req, res) => {
+    let env = req.query.env;
+    if(!env){
+        env = config.nodeEnv;
+    }
+    console.log(req.body);
+    try {
+        fs.writeFileSync(`./infrastructure.${env}.yaml`, yaml.dump(req.body));
+        return res.status(201).json(req.body);
+    } catch(err) {
+        console.log("Failed to save infrastructure file");
+        return res.status(500).send('Failed to save infrastructure configuration');
+    }
+});
     
 app.use('/api/v1', proxy);
 
